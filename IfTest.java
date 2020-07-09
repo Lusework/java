3de5727bcd8250f0535d7ad5fa9858337c1758e0
Lusework <38586586+Lusework@users.noreply.github.com>
2020-07-09 20:00:40 +0800

    Add files via upload

---
+++ IfTest.java
@@ -0,0 +1,18 @@
+
+public class IfTest {
+	public static void main(String args[])
+	{
+		int i = 12;
+		if(i<12)
+		{
+			System.out.println("´í");
+		}
+		//if(i>12)
+		{
+			System.out.println("´í");
+		}
+		
+		
+	}
+
+}
